7b9e880da4f4a2af75eef1188a6183f0d15ca60b
Vladimir <cbb110@rambler.ru>
2023-09-26 17:26:44 +0300

    Add char

---
+++ DoubleDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+namespace SimpleProject
+{
+    public delegate T AddDelegate<T>(T x, T y);
+    public class ExampleClass
+    {
+        public int AddInt(int x, int y)
+        {
+            return x + y;
+        }
+        public double AddDouble(double x, double y)
+        {
+            return x + y;
+        }
+        public static char AddChar(char x, char y)
+        {
+            return (char)(x + y);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleClass example = new ExampleClass();
+            AddDelegate<int> delInt = example.AddInt;
+            WriteLine($"The sum of integers: {delInt(8, 6)}");
+            AddDelegate<double> delDouble =  example.AddDouble;
+            WriteLine($"The sum of real numbers:{ delDouble(45.67, 62.81)}");
+            AddDelegate<char> delChar = ExampleClass.AddChar;
+            WriteLine($"The sum characters:{ delChar('S', 'h')}");
+        }
+    }
+}
